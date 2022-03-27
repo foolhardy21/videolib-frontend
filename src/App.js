98b029f4c48efbfe2ca69d267f3cc2d8fa267c72
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-27 17:23:39 +0530

    fix - videos and filters providers imported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { Signup, Login, Videos } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup, Login, Videos } from "./pages";
+import { SignupProvider, LoginProvider } from './contexts'
 
 const App = () => {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={
+          <SignupProvider>
+            <Signup />
+          </SignupProvider>
+        } />
+        <Route path='/login' element={
+          <LoginProvider>
+            <Login />
+          </LoginProvider>} />
         <Route path='/videos' element={<Videos />} />
       </Routes>
     </BrowserRouter>
