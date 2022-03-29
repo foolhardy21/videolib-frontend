8c95d91e4fffbca7f74bfe6a97645975d84f65ec
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-29 14:09:38 +0530

    style - alert and loading set for likes section

--- src/pages/Playlists.js
+++ src/pages/Playlists.js
@@ -1,19 +1,13 @@
 import { useEffect } from 'react'
 import { PlaylistHeader, PlaylistsSection } from "../components/Playlists"
-import { Text, Main, Alert } from "../components/Reusable"
+import { Text, Main } from "../components/Reusable"
 import { useLikes, useTheme } from "../contexts"
 import { getBgColor, getTextColor } from "../utils"
 import '../components/Playlists/playlists.css'
 
 const Playlists = () => {
     const { theme } = useTheme()
-    const { likesState: {
-        loading,
-        alert: {
-            message,
-            type
-        }
-    }, likesDispatch, getLikedVideos, showLikesAlert } = useLikes()
+    const { likesDispatch, getLikedVideos, showLikesAlert } = useLikes()
 
     useEffect(() => {
         (async () => {
@@ -39,21 +33,7 @@ const Playlists = () => {
 
                 <Text classes={`txt-lg txt-cap ${getTextColor(theme)} mg-top-md mg-btm-md`}>all playlists</Text>
 
-                <div className='flx flx-center'>
-
-                    {
-                        type === 'error'
-                            ? <Alert classes='bg-err'>{message}</Alert>
-                            : type === 'success' ? <Alert classes='bg-success'>{message}</Alert>
-                                : ''
-                    }
-                </div>
-
-                {
-                    loading
-                        ? <Text classes={`${getTextColor(theme)} txt-xlg txt-500 txt-cap`}>loading...</Text>
-                        : <PlaylistsSection />
-                }
+                <PlaylistsSection />
 
             </Main>
 
