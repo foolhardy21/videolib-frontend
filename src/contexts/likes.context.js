efd1dcf7cca01a56bcad4c6b18148b5bd650bfb3
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-29 14:02:48 +0530

    refactor - reducer file for likes context

--- src/contexts/likes.context.js
+++ src/contexts/likes.context.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { createContext, useContext, useReducer } from 'react'
 import { useAuth } from './auth.context'
+import { likesReducer } from '../reducers'
 
 const LikesContext = createContext()
 
@@ -70,39 +71,6 @@ export const LikesProvider = ({ children }) => {
     }
 
     const isVideoLiked = _id => likesState.likedVideos.some(likedVideo => likedVideo._id === _id)
-
-    function likesReducer(state, action) {
-
-        switch (action.type) {
-
-            case 'INIT_LIKES': return { ...state, likedVideos: action.payload }
-
-            case 'SET_ALERT': return {
-                ...state, alert: {
-                    message: action.payload.message,
-                    type: action.payload.type
-                }
-            }
-
-            case 'REMOVE_ALERT': return {
-                ...state, alert: {
-                    message: '',
-                    type: ''
-                }
-            }
-
-            case 'SET_LOADING': return { ...state, loading: true }
-
-            case 'REMOVE_LOADING': return { ...state, loading: false }
-
-            case 'ADD_TO_LIKES': return { ...state, likedVideos: state.likedVideos.concat({ ...action.payload }) }
-
-            case 'REMOVE_FROM_LIKES': return { ...state, likedVideos: state.likedVideos.filter(video => video._id !== action.payload) }
-
-            default: return state
-
-        }
-    }
 
     return (
         <LikesContext.Provider
