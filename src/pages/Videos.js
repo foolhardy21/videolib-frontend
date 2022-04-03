5b4d6e461ab9fcc3229721612c02e527e1f307ab
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-04-03 21:09:18 +0530

    feat - redirect to videos page with category filter

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import BarLoader from "react-spinners/BarLoader";
 import { VideosFilter, VideosHeader, VideosSection, PlaylistModal } from 'components/Videos'
 import { Alert, Main, Text } from 'components/Reusable'
-import { usePlaylists, useTheme, useVideos } from 'contexts'
+import { useFilter, usePlaylists, useTheme, useVideos } from 'contexts'
 import { useTitle } from 'hooks/useTitle'
 import { getBgColor, getTextColor } from 'utils'
 import { ACTION_INIT_VIDEOS, ALERT_TYPE_ERROR, ALERT_TYPE_SUCCESS } from 'utils/constants.util'
@@ -18,13 +18,14 @@ const Videos = () => {
         loading
     }, videosDispatch, getVideos, showVideosAlert } = useVideos()
     const { isPlaylistModalVisible } = usePlaylists()
+    const { filterState } = useFilter()
 
     useEffect(() => {
         (async () => {
             const videos = await getVideos()
             if (videos === 404 || videos === 500) {
                 showVideosAlert('could not fetch the videos', ALERT_TYPE_ERROR)
-            } else {
+            } else if (filterState.length === 0) {
                 videosDispatch({ type: ACTION_INIT_VIDEOS, payload: videos })
             }
         })()
